393a3ce1b7e9c4a9fa41856c667091ddd2831442
eric.tan <eric.tan@smartsparrow.com>
2019-08-04 11:16:35 -0700

    ajax goodies

--- js/application.js
+++ js/application.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {	
-	console.log('ready');
+	var $input = $('#url');
+	$input.val('http://routehelperserviceelb-1812164016.us-west-2.elb.amazonaws.com:1212/version/');
+	
+	var $results = $('#results');
+	
+	var $button = $('#button');
+	$button.on('click', function() {
+		$.ajax($input.val()).then(function(json) {
+			appendResults($results, JSON.stringify(json, null, 4));
+		});
+	});
+	
+	function appendResults($div, val) {
+		var $result = $('<div class="result">' + val + '</div>').prependTo($div);
+		setTimeout(function() {
+			$result.fadeOut(1000, function() {
+				$result.remove();
+			});
+		}, 2000);
+	}
 });
